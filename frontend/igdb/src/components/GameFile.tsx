de08cfc496bd6b9121b1b7d9f207195e8b7a4813
R4NK <nahumoscatelli@gmail.com>
2019-11-01 14:28:02 -0300

    Agregado React-Router y soportes varios para una estructura multipágina

---
+++ frontend/igdb/src/components/GameFile.tsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class GameFile extends Component{
+    render(){
+        return(
+            <div>
+                Hola, soy un GameFile
+            </div>
+        );
+    }
+}
