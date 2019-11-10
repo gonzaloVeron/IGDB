a45af3102c2d7b5a79e577a34f268604f02ad56e
GonzaloVeron <gonveron96@gmail.com>
2019-11-10 19:49:49 -0300

    agrego devFile y devStudioFile, modifico gameFile

--- frontend/igdb/src/components/gamefile/GameFile.js
+++ frontend/igdb/src/components/gamefile/GameFile.js
@@ -9,6 +9,7 @@ export default class GameFile extends React.Component {
         super(props);
         this.state = {
             gameData: {
+                id: -999,
                 nombre: '',
                 sinopsis: '',
                 genero: '',
@@ -20,8 +21,8 @@ export default class GameFile extends React.Component {
 
     
     componentDidMount(){
-        let { gameName } = this.props.match.params
-        getGame(gameName).then(result => {
+        let { gameID } = this.props.match.params
+        getGame(gameID).then(result => {
             this.setState({ gameData : result });
             console.log(result)
         }).catch(e => {this.setState({ error: e.message })})
